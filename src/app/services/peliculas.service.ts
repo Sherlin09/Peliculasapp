346726045a775bb329bc74c85f07f97d785bdb92
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2020-10-14 17:51:59 -0500

    Obtener peliculas en cartelera

---
+++ src/app/services/peliculas.service.ts
@@ -0,0 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CarteleraResponse } from '../interfaces/cartelera-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeliculasService {
+
+  constructor( private http: HttpClient ) { }
+
+  getCartelera():Observable<CarteleraResponse> {
+
+    return this.http.get<CarteleraResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=6fc1db2f2f1af0ba9465f16ff2b658cb&language=es-ES&page=1');
+  }
+}
